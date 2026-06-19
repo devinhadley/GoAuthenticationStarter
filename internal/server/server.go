01ff5ba8fef440c9100dbfff29634bd0a34f5e84
Devin Hadley <68879608+devinhadley@users.noreply.github.com>
2026-06-19 09:55:42 -0700

    refactor(server): group user handlers

--- internal/server/server.go
+++ internal/server/server.go
@@ -12,12 +12,12 @@ import (
 func NewMux(userService *user.Service, sessionService *session.Service) http.Handler {
 	mux := http.NewServeMux()
 
+	mux.Handle("GET /user", handlers.CreateGetUserHandler())
 	mux.Handle("POST /user/signup", handlers.CreateSignUpHandler(userService, sessionService))
 	mux.Handle("POST /user/login", handlers.CreateLoginHandler(userService, sessionService))
 	mux.Handle("PUT /user/password", handlers.CreateAuthenticatedPasswordResetHandler(userService))
 	mux.Handle("POST /password-reset", handlers.CreatePasswordResetRequestHandler(userService))
 	mux.Handle("PUT /password-reset", handlers.CreateTokenPasswordResetHandler(userService))
-	mux.Handle("GET /user", handlers.CreateGetUserHandler())
 
 	return middleware.CreateSessionMiddleware(userService, sessionService, mux)
 }
